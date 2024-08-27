77b2e067586aa6b2f5c471fb383f01060e351fcb
CHINMAYEE PATIL <155649544+2003chinmayee@users.noreply.github.com>
2024-08-27 07:44:52 +0530

    Add files via upload

---
+++ practice1/script.js
@@ -0,0 +1,19 @@
+gsap.to(".box1" , {
+    x:1200,
+    duration:2,
+    delay:1,
+    scale:.5,
+    rotate:360,
+    borderRadius:"50%",
+    backgroundColor:"yellow"
+})
+
+gsap.from(".box2" , {
+    x:1200,
+    duration:2,
+    delay:1,
+    scale:.5,
+    rotate:360,
+    borderRadius:"50%",
+    backgroundColor:"red"
+})
